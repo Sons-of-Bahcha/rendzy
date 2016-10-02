a0ce6d894a3c385b334a88dced786521d098069d
ROOT <azeriker@gmail.com>
2016-10-02 23:18:05 +0600

    Table setters and getters

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -2,9 +2,25 @@
  * Created by ROOT on 02.10.2016.
  */
 public class Table {
+    
     private int[][] table;
-    Table()
+    public Table()
     {
         table =new int[15][15];
+    }
+    public void setCell(int x,int y, int status)
+    {
+        table[x][y]=status;
+    }
+    public int getStatusInCell(int x, int y)
+    {
+        return  table[x][y];
+    }
+    public int[][] getTable() {
+        return table;
+    }
+
+    public void setTable(int[][] table) {
+        this.table = table;
     }
 }
