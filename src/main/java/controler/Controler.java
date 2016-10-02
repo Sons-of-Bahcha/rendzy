4f1eec5f9d1566c2027d9fea169ded611e01ca67
Viteker <harvetal@gmail.com>
2016-10-02 23:04:13 +0600

    В игровой loop добавлен вызов к перерисовке окна

--- src/main/java/controler/Controler.java
+++ src/main/java/controler/Controler.java
@@ -1,5 +1,7 @@
 package controler;
 
+import gui.Window;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,12 +10,14 @@ import java.awt.event.ActionListener;
  * Created by 123 on 02.10.2016.
  */
 public class Controler implements ActionListener {
-    public Controler(){
+    private Window w;
+    public Controler(Window w){
+        this.w=w;
         Timer t=new Timer(20,this);
         t.start();
     }
 
     public void actionPerformed(ActionEvent e) {
-
+        w.repaint();
     }
 }
