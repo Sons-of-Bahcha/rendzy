76fece668c025d0c8a40d35134a76e17a12485f4
Viteker <harvetal@gmail.com>
2016-10-02 23:09:24 +0600

    Начальный этап для рисования игрового поля с двойной буфферизацией

--- src/main/java/gui/Window.java
+++ src/main/java/gui/Window.java
@@ -1,7 +1,26 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 /**
  * Created by 123 on 02.10.2016.
  */
-public class Window {
+public class Window extends JFrame {
+    BufferedImage map;
+    public Window(int width,int height,String s){
+        super(s);
+        setSize(width, height);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+    private Window(int width,int height){
+        this.map=new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+        setVisible(true);
+    }
+    public void paint(Graphics g){
+        g.drawImage(map,0,0,null);
+    }
 }
