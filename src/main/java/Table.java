db4204b8251a44579c6418a89844aac08f75f3ab
ROOT <azeriker@gmail.com>
2016-10-02 23:20:27 +0600

    теперь контроллер написано без ошибок

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -2,7 +2,7 @@
  * Created by ROOT on 02.10.2016.
  */
 public class Table {
-    
+
     private int[][] table;
     public Table()
     {
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,7 +1,26 @@
 package controller;
 
+import gui.Window;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by 123 on 02.10.2016.
  */
-public class Controller {
+
+public class Controller implements ActionListener {
+    private Window w;
+    public Controller(Window w){
+        this.w=w;
+        Timer t=new Timer(20,this);
+        t.start();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        w.repaint();
+    }
 }
+
+
