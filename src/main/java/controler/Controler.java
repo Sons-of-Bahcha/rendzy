34b018d64896c0c464e2667d966ebcd04dc15266
Viteker <harvetal@gmail.com>
2016-10-02 23:01:20 +0600

    Игровой цикл

--- src/main/java/controler/Controler.java
+++ src/main/java/controler/Controler.java
@@ -1,7 +1,19 @@
 package controler;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by 123 on 02.10.2016.
  */
-public class Controler {
+public class Controler implements ActionListener {
+    public Controler(){
+        Timer t=new Timer(20,this);
+        t.start();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
