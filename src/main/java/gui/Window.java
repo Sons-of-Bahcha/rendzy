031284aa9ecf7d85ef36683ac1b937d0b2d45379
Viteker <harvetal@gmail.com>
2016-10-04 19:30:34 +0600

    Дополнил класс window, теперь можно создавать объект этого класса в котороом просто отрисовывается белый фон

--- src/main/java/gui/Window.java
+++ src/main/java/gui/Window.java
@@ -5,26 +5,57 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 /**
- * Created by 123 on 02.10.2016.
+ * Класс представляющий из себя главное окно
+ * Created by Viteker on 02.10.2016.
+ * @author Viteker
+ * @version 1.2
  */
 public class Window extends JFrame {
-    BufferedImage map;
+    private final BufferedImage bg;//главный фон
+
+    /**
+     * Стартовый конструктор, в котором создается окно с указанными размерами и title для него устанавливаетя строчкой s
+     * @param width - ширина окна
+     * @param height - высота окна
+     * @param s - title
+     */
     public Window(int width, int height, String s){
-        super(s);
+        this(width,height);
+        setTitle(s);
         setSize(width, height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-    }
-    private Window(int width, int height){
-        this.map=new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
-        Graphics g=map.getGraphics();
 
         setVisible(true);
     }
-    
-    public void paint(Graphics g){
-        g.drawImage(map,0,0,null);
 
+    /**
+     * Второстепенный конструктор который вызывается из главного, в котором просто подготавливает фон окна
+     * @param width - ширина фона (ширина фона совпадает с шириной окна)
+     * @param height - высота фона (высота фона совпадает с высотой окна)
+     */
+    private Window(int width, int height){
+        super();
+        this.bg=new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+        prepareBackground();
+
+    }
+
+    /**
+     * Подготавливаем белый фон для окна
+     */
+    private void prepareBackground(){
+        Graphics g=bg.getGraphics();
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(0,0,bg.getWidth(),bg.getHeight());
+    }
+
+    /**
+     * Отрисовка фона окна
+     * @param g - графический рбъекта на главного окна {@link Window}
+     */
+    public void paint(Graphics g){
+        g.drawImage(bg,0,0,null);
     }
 }
